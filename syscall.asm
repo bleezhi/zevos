2b9d083be1e9762d9c1673c4b7c32d8d96de144b
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-15 21:32:46 +0200

    nightly: add syscall entry foundation

---
+++ syscall.asm
@@ -0,0 +1,32 @@
+BITS 64
+section .text
+global syscall_entry
+extern syscall_dispatch
+
+syscall_entry:
+    /* Early syscall ABI foundation: RAX is the syscall number.
+     * Arguments and full register preservation will be added with
+     * the real userspace process ABI.
+     */
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov rdi, rax
+    call syscall_dispatch
+    mov r12, rax
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    mov rax, r12
+    iretq
+
+section .note.GNU-stack noalloc noexec nowrite progbits
