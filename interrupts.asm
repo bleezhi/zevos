9140f0d80811e03a0c1608f37a63586f4806dca2
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-15 20:47:59 +0200

    Add x86_64 interrupt entry stubs

---
+++ interrupts.asm
@@ -0,0 +1,159 @@
+; ZevOS interrupt entry stubs
+BITS 64
+
+section .text
+extern exception_handler
+extern irq_handler
+
+global isr0, isr1, isr2, isr3, isr4, isr5, isr6, isr7
+global isr8, isr9, isr10, isr11, isr12, isr13, isr14, isr15
+global isr16, isr17, isr18, isr19, isr20, isr21, isr22, isr23
+global isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31
+global irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7
+global irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15
+
+%macro EXCEPTION_NOERR 1
+isr%1:
+    push qword 0
+    push qword %1
+    jmp exception_common
+%endmacro
+
+%macro EXCEPTION_ERR 1
+isr%1:
+    push qword %1
+    jmp exception_common
+%endmacro
+
+%macro IRQ 2
+irq%1:
+    push qword 0
+    push qword %2
+    jmp irq_common
+%endmacro
+
+exception_common:
+    cld
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    mov rdi, rsp
+    call exception_handler
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+    add rsp, 16
+    iretq
+
+irq_common:
+    cld
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    mov rdi, rsp
+    call irq_handler
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+    add rsp, 16
+    iretq
+
+EXCEPTION_NOERR 0
+EXCEPTION_NOERR 1
+EXCEPTION_NOERR 2
+EXCEPTION_NOERR 3
+EXCEPTION_NOERR 4
+EXCEPTION_NOERR 5
+EXCEPTION_NOERR 6
+EXCEPTION_NOERR 7
+EXCEPTION_ERR   8
+EXCEPTION_NOERR 9
+EXCEPTION_ERR   10
+EXCEPTION_ERR   11
+EXCEPTION_ERR   12
+EXCEPTION_ERR   13
+EXCEPTION_ERR   14
+EXCEPTION_NOERR 15
+EXCEPTION_NOERR 16
+EXCEPTION_ERR   17
+EXCEPTION_NOERR 18
+EXCEPTION_NOERR 19
+EXCEPTION_NOERR 20
+EXCEPTION_NOERR 21
+EXCEPTION_NOERR 22
+EXCEPTION_NOERR 23
+EXCEPTION_NOERR 24
+EXCEPTION_NOERR 25
+EXCEPTION_NOERR 26
+EXCEPTION_NOERR 27
+EXCEPTION_NOERR 28
+EXCEPTION_NOERR 29
+EXCEPTION_ERR   30
+EXCEPTION_NOERR 31
+
+IRQ 0, 32
+IRQ 1, 33
+IRQ 2, 34
+IRQ 3, 35
+IRQ 4, 36
+IRQ 5, 37
+IRQ 6, 38
+IRQ 7, 39
+IRQ 8, 40
+IRQ 9, 41
+IRQ 10, 42
+IRQ 11, 43
+IRQ 12, 44
+IRQ 13, 45
+IRQ 14, 46
+IRQ 15, 47
+
+section .note.GNU-stack noalloc noexec nowrite progbits
