81f933938de666be8fd1147f40fd1ec8726dfa5f
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-15 19:24:00 +0200

    Connect PS/2 keyboard to shell

--- keyboard.c
+++ keyboard.c
@@ -1,8 +1,14 @@
+/* ZevOS PS/2 keyboard driver */
+
 #define VGA ((volatile unsigned short *)0xB8000)
 #define DATA 0x60
 #define STATUS 0x64
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
 
 static unsigned int cursor = 0;
+
+void shell_input(char c);
 
 static unsigned char inb(unsigned short port)
 {
@@ -11,26 +17,58 @@ static unsigned char inb(unsigned short port)
     return value;
 }
 
-static const char keys[] = "\0\0" "1234567890-=\b\t" "qwertyuiop[]\n" "\0asdfghjkl;'`" "\0\\zxcvbnm,./";
-
-static void putchar(char c)
+void terminal_putchar(char c)
 {
-    if (c == '\n') { cursor = (cursor / 80 + 1) * 80; return; }
-    if (c == '\b') { if (cursor) { --cursor; VGA[cursor] = 0x0F20; } return; }
-    if (cursor < 2000) VGA[cursor++] = (unsigned short)c | 0x0F00;
+    if (c == '\n') {
+        cursor = (cursor / VGA_WIDTH + 1) * VGA_WIDTH;
+    } else {
+        if (cursor >= VGA_WIDTH * VGA_HEIGHT)
+            cursor = (VGA_HEIGHT - 1) * VGA_WIDTH;
+        VGA[cursor++] = (unsigned short)c | 0x0F00;
+    }
+
+    if (cursor >= VGA_WIDTH * VGA_HEIGHT)
+        cursor = (VGA_HEIGHT - 1) * VGA_WIDTH;
 }
+
+void terminal_puts(const char *s)
+{
+    while (*s)
+        terminal_putchar(*s++);
+}
+
+void terminal_backspace(void)
+{
+    if (cursor > 0) {
+        --cursor;
+        VGA[cursor] = 0x0F20;
+    }
+}
+
+void terminal_clear(void)
+{
+    for (unsigned int i = 0; i < VGA_WIDTH * VGA_HEIGHT; ++i)
+        VGA[i] = 0x0F20;
+    cursor = 0;
+}
+
+static const char keys[] = "\0\0" "1234567890-=\b\t" "qwertyuiop[]\n" "\0asdfghjkl;'`" "\0\\zxcvbnm,./";
 
 void keyboard_init(void)
 {
-    const char *s = "ZevOS v0.1\nPS/2 keyboard ready. Type something!\n> ";
-    while (*s) putchar(*s++);
+    /* The shell owns the terminal banner. */
 }
 
 void keyboard_poll(void)
 {
     while (inb(STATUS) & 1) {
         unsigned char sc = inb(DATA);
-        if (sc & 0x80) continue;
-        if (sc < sizeof(keys) - 1 && keys[sc]) putchar(keys[sc]);
+
+        /* Ignore key-release scancodes for now. */
+        if (sc & 0x80)
+            continue;
+
+        if (sc < sizeof(keys) - 1 && keys[sc])
+            shell_input(keys[sc]);
     }
 }
