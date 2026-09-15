ff15445b9b4bfcaf1ec88455e0191e62c1c5fb34
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-15 21:24:03 +0200

    Add ring 3 transition helper

---
+++ usermode.asm
@@ -0,0 +1,26 @@
+; ZevOS ring 3 transition helper
+BITS 64
+
+section .text
+global user_enter
+
+; void user_enter(entry, stack, user_cs, user_ss)
+; RDI=entry, RSI=stack, RDX=user_cs, RCX=user_ss
+user_enter:
+    mov ax, cx
+    mov ds, ax
+    mov es, ax
+
+    ; Build the frame consumed by iretq:
+    ; SS, RSP, RFLAGS, CS, RIP.
+    push rcx
+    push rsi
+    pushfq
+    pop rax
+    or rax, 0x200
+    push rax
+    push rdx
+    push rdi
+    iretq
+
+section .note.GNU-stack noalloc noexec nowrite progbits
