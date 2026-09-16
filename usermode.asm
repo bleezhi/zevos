b4396ace8547d79a5501460751e9c11e69daf46f
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 06:28:52 +0200

    nightly: add combined user CR3 and iret launcher

--- usermode.asm
+++ usermode.asm
@@ -1,8 +1,9 @@
-; ZevOS ring 3 transition helper
+; ZevOS ring 3 transition helpers
 BITS 64
 
 section .text
 global user_enter
+global user_launch
 
 ; void user_enter(entry, stack, user_cs, user_ss)
 ; RDI=entry, RSI=stack, RDX=user_cs, RCX=user_ss
@@ -11,8 +12,6 @@ user_enter:
     mov ds, ax
     mov es, ax
 
-    ; Build the frame consumed by iretq:
-    ; SS, RSP, RFLAGS, CS, RIP.
     push rcx
     push rsi
     pushfq
@@ -21,6 +20,28 @@ user_enter:
     push rax
     push rdx
     push rdi
+    iretq
+
+; void user_launch(cr3, entry, stack)
+; RDI=CR3, RSI=user RIP, RDX=user RSP.
+; Builds the complete initial privilege-transition frame and never returns.
+user_launch:
+    mov cr3, rdi
+
+    mov ax, 0x23
+    mov ds, ax
+    mov es, ax
+
+    ; Initial ring-3 interrupt-return frame:
+    ; SS, RSP, RFLAGS, CS, RIP.
+    push qword 0x23
+    push rdx
+    pushfq
+    pop rax
+    or rax, 0x200
+    push rax
+    push qword 0x1B
+    push rsi
     iretq
 
 section .note.GNU-stack noalloc noexec nowrite progbits
