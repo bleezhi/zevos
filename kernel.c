d84922089041c97b921a2454a0292abb891813e2
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 15:16:30 +0200

    Report first userspace creation failure stage

--- kernel.c
+++ kernel.c
@@ -1,5 +1,7 @@
 /* ZevOS kernel - freestanding C */
 
+#include <stdint.h>
+
 void terminal_init(void);
 void terminal_puts(const char *s);
 void idt_init(void);
@@ -12,7 +14,16 @@ void vmm_init(void);
 void vfs_init(void);
 struct process;
 struct process *process_create_first_user(void);
+uint32_t process_first_user_error(void);
 void process_launch_user(struct process *process);
+
+static void print_digit(uint32_t value)
+{
+    char s[2];
+    s[0] = (char)('0' + (value % 10));
+    s[1] = 0;
+    terminal_puts(s);
+}
 
 void kernel_main(unsigned int multiboot_magic, unsigned int multiboot_info)
 {
@@ -50,7 +61,6 @@ void kernel_main(unsigned int multiboot_magic, unsigned int multiboot_info)
     shell_init();
     terminal_puts("ZevOS: kernel foundation ready\n");
 
-    /* Combined userspace smoke test. */
     struct process *first_user = process_create_first_user();
     if (first_user) {
         terminal_puts("userspace: first process image ready\n");
@@ -58,7 +68,9 @@ void kernel_main(unsigned int multiboot_magic, unsigned int multiboot_info)
         process_launch_user(first_user);
     }
 
-    terminal_puts("userspace: failed to create first process\n");
+    terminal_puts("userspace: failed to create first process (stage ");
+    print_digit(process_first_user_error());
+    terminal_puts(")\n");
     __asm__ volatile ("sti");
     for (;;) __asm__ volatile ("hlt");
 }
