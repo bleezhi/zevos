e90317eab43d2068369e2f7a36e0c046fb0d57eb
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 15:35:15 +0200

    syscalls: connect read write open close exit

--- syscall.c
+++ syscall.c
@@ -2,22 +2,27 @@
 
 #include <stdint.h>
 
-void terminal_puts(const char *s);
+int fd_open(uint64_t user_path);
+int fd_close(int fd);
+int fd_read(int fd, uint64_t user_buffer, uint64_t count);
+int fd_write(int fd, uint64_t user_buffer, uint64_t count);
 
-uint64_t syscall_dispatch(uint64_t number, uint64_t arg1, uint64_t arg2)
+uint64_t syscall_dispatch(uint64_t number, uint64_t arg1,
+                          uint64_t arg2, uint64_t arg3)
 {
-    (void)arg2;
-
     switch (number) {
-    case 0:
+    case 0: /* read(fd, buffer, count) */
+        return (uint64_t)fd_read((int)arg1, arg2, arg3);
+    case 1: /* write(fd, buffer, count) */
+        return (uint64_t)fd_write((int)arg1, arg2, arg3);
+    case 2: /* open(path, flags) -- flags are currently ignored */
+        (void)arg2;
+        return (uint64_t)fd_open(arg1);
+    case 3: /* exit(status) -- process teardown is the next scheduler step */
+        (void)arg1;
         return 0;
-
-    case 1:
-        /* Early write syscall: arg1 is a NUL-terminated user string. */
-        if (arg1 >= 0x400000ULL && arg1 < 0x800000ULL)
-            terminal_puts((const char *)arg1);
-        return 0;
-
+    case 4: /* close(fd) */
+        return (uint64_t)fd_close((int)arg1);
     default:
         return (uint64_t)-1;
     }
