56d8b350a4ce1fe25d80fcc72895fc70dca1fccf
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-17 15:37:49 +0200

    Add /bin/false userspace source

---
+++ bin/false.asm
@@ -0,0 +1,25 @@
+BITS 64
+org 0x400000
+
+db 0x7f,'ELF',2,1,1,0
+ times 8 db 0
+ dw 2,0x3e
+ dd 1
+ dq _start
+ dq phdr-$$
+ dq 0
+ dd 0
+ dw 64,56,1,0,0,0
+phdr:
+ dd 1,5
+ dq 0
+ dq $$
+ dq $$
+ dq end-$$
+ dq end-$$
+ dq 0x1000
+_start:
+ mov eax,3
+ mov edi,1
+ int 0x80
+end:
