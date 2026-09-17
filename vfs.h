559d61188c3b81f98986c6a19a3bbd8ea0e8e23a
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-17 15:25:56 +0200

    Add raw VFS I/O for executable files

--- vfs.h
+++ vfs.h
@@ -13,8 +13,11 @@ int vfs_mkdir(const char *path);
 int vfs_touch(const char *path);
 int vfs_remove(const char *path);
 int vfs_read(const char *path, char *buffer, uint64_t size);
+int vfs_read_binary(const char *path, void *buffer, uint64_t size);
 int vfs_write(const char *path, const char *data, uint64_t size);
+int vfs_write_binary(const char *path, const void *data, uint64_t size);
 int vfs_copy(const char *source, const char *destination);
 int vfs_list(const char *path, void (*emit)(const char *name, int type));
+void vfs_install_binaries(void);
 
 #endif
