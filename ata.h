8695a6263179c2192bd644fa487b48e5a5a28609
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-17 16:22:18 +0200

    storage: add ATA PIO interface

---
+++ ata.h
@@ -0,0 +1,8 @@
+#ifndef ZEVOS_ATA_H
+#define ZEVOS_ATA_H
+#include <stdint.h>
+int ata_init(void);
+int ata_read28(uint32_t lba, uint8_t *buffer);
+int ata_write28(uint32_t lba, const uint8_t *buffer);
+int ata_present(void);
+#endif
