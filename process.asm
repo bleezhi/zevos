fed8ed64c36a2c5bf4ecb3f7e5a8c43b77423a74
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-17 06:42:32 +0200

    Add process exit handoff

--- process.asm
+++ process.asm
@@ -1,8 +1,13 @@
-; ZevOS kernel context switch
+; ZevOS kernel process context helpers
 BITS 64
 
 section .text
 global process_switch
+global process_exit_handoff
+
+extern vmm_kernel_cr3
+extern process_exit_idle
+extern tss_stack_top
 
 ; void process_switch(uint64_t *old_rsp, uint64_t new_rsp)
 ; Save the SysV callee-saved registers and switch to another kernel stack.
@@ -21,8 +26,25 @@ process_switch:
     pop r14
     pop r13
     pop r12
-    pop rbx
     pop rbp
     ret
+
+; void process_exit_handoff(uint64_t status)
+; Abandon the terminated task's ring-0 stack, restore the kernel address
+; space, and continue on the dedicated bootstrap/TSS stack.
+process_exit_handoff:
+    push rdi
+    call vmm_kernel_cr3
+    mov cr3, rax
+    pop rdi
+
+    mov rsp, tss_stack_top
+    and rsp, -16
+    call process_exit_idle
+
+.hang:
+    cli
+    hlt
+    jmp .hang
 
 section .note.GNU-stack noalloc noexec nowrite progbits
