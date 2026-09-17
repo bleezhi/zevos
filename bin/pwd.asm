3786c279e89a62c86f7111caaffdf4f89b76b0a3
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-17 15:37:56 +0200

    Add /bin/pwd userspace source

---
+++ bin/pwd.asm
@@ -0,0 +1,32 @@
+BITS 64
+org 0x400000
+
+db 0x7f,'ELF',2,1,1,0
+ times 8 db 0
+ dw 2,0x3e
+ dd 1
+ dq _start
+ dq phdr-$$
+ dq 0
+ dd 0
+ dw 64,56,1,0,0,0
+phdr:
+ dd 1,5
+ dq 0
+ dq $$
+ dq $$
+ dq end-$$
+ dq end-$$
+ dq 0x1000
+_start:
+ mov eax,1
+ mov edi,1
+ mov rsi,msg
+ mov edx,msg_end-msg
+ int 0x80
+ mov eax,3
+ xor edi,edi
+ int 0x80
+msg db '/',10
+msg_end:
+end:
