6e801b6bb5bc14adfb9ba4e9e26044e9742fa4c8
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 16:22:10 +0200

    Remove obsolete HDA boot embedding

--- hda_boot_embed.asm
+++
@@ -1,9 +0,0 @@
-BITS 64
-section .rodata
-align 16
-global hda_boot_start
-global hda_boot_end
-hda_boot_start:
-    incbin "hda_boot.bin"
-hda_boot_end:
-section .note.GNU-stack noalloc noexec nowrite progbits
