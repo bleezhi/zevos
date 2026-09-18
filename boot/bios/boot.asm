9a66040ef7a2069b3264a4096d204b6a01c94060
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 16:24:13 +0200

    ZevBoot BIOS: support El Torito boot-image LBA

--- boot/bios/boot.asm
+++ boot/bios/boot.asm
@@ -3,6 +3,16 @@
 BITS 16
 ORG 0x7C00
 
+; El Torito Boot Info Table is patched by xorriso at offset 8.
+; Offset 12 contains the LBA of this boot image. For HDA it remains zero.
+times 8-($-$) db 0
+boot_info_table:
+    dd 0
+    dd 0
+    dd 0
+    dd 0
+    times 40 db 0
+
 start:
     cli
     xor ax, ax
@@ -12,12 +22,17 @@ start:
     mov sp, 0x7C00
     mov [boot_drive], dl
 
+    mov eax, [boot_info_table + 4]
+    mov [0x7BF0], eax
+
     mov word [dap.count], 16
     mov word [dap.offset], 0x8000
     mov word [dap.segment], 0
     mov dword [dap.buffer_lo], 0x00008000
     mov dword [dap.buffer_hi], 0
-    mov dword [dap.lba_lo], 2
+    mov eax, [0x7BF0]
+    add eax, 2
+    mov [dap.lba_lo], eax
     mov dword [dap.lba_hi], 0
 
     mov si, dap
