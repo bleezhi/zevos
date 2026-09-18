2c2d8c6ceb776d2de86dd132dd4d08d6b63acf38
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-18 16:19:54 +0200

    ZevBoot: switch kernel to native handoff

--- boot.asm
+++ boot.asm
@@ -1,22 +1,9 @@
-; ZevOS - Multiboot2 x86_64 entry point
-; GRUB enters us in 32-bit protected mode. This trampoline enables
-; PAE + long mode, installs identity-mapped 2 MiB pages, then calls C.
-; Direct HDA boot enters the same 32-bit entry with the Multiboot magic
-; supplied by hda_boot.asm and a null info pointer.
-
 BITS 32
 
-section .multiboot
-align 8
-mb2_header:
-    dd 0xE85250D6
-    dd 0
-    dd mb2_header_end - mb2_header
-    dd -(0xE85250D6 + 0 + (mb2_header_end - mb2_header))
-    dw 0
-    dw 0
-    dd 8
-mb2_header_end:
+; ZevOS native BIOS kernel trampoline.
+; ZevBoot BIOS stage 2 enters here in 32-bit protected mode with:
+;   EBX = struct zev_boot_info *
+; The trampoline creates a 64-bit environment and calls kernel_main(info).
 
 section .text
 align 16
@@ -29,8 +16,7 @@ extern kernel_bss_end
 _start:
     cli
     mov esp, stack_top
-    mov [multiboot_magic], eax
-    mov [multiboot_info], ebx
+    mov [boot_info_ptr], ebx
 
     mov eax, page_table_l4
     mov cr3, eax
@@ -39,9 +25,16 @@ _start:
     or eax, 0x3
     mov [page_table_l4], eax
 
+    xor ecx, ecx
     mov eax, page_table_l2
-    or eax, 0x3
-    mov [page_table_l3], eax
+.fill_l3:
+    mov edx, eax
+    or edx, 0x3
+    mov [page_table_l3 + ecx * 8], edx
+    add eax, 4096
+    inc ecx
+    cmp ecx, 4
+    jne .fill_l3
 
     xor ecx, ecx
 .fill_pd:
@@ -51,7 +44,7 @@ _start:
     mov [page_table_l2 + ecx * 8], eax
     mov dword [page_table_l2 + ecx * 8 + 4], 0
     inc ecx
-    cmp ecx, 512
+    cmp ecx, 2048
     jne .fill_pd
 
     mov eax, cr4
@@ -80,8 +73,6 @@ long_mode_start:
     mov rsp, stack_top
     and rsp, -16
 
-    ; GRUB normally gives us zeroed BSS. Direct HDA boot does not, so
-    ; explicitly clear it before any global/BSS-backed subsystem runs.
     cld
     mov rdi, kernel_bss_start
     mov rcx, kernel_bss_end
@@ -92,8 +83,7 @@ long_mode_start:
 
     call tss_init
 
-    mov edi, dword [multiboot_magic]
-    mov esi, dword [multiboot_info]
+    mov rdi, [rel boot_info_ptr]
     call kernel_main
 
 .hang:
@@ -107,27 +97,31 @@ global gdt64
 gdt64:
     dq 0
 .code equ $ - gdt64
-    dq 0x00AF9A000000FFFF       ; kernel code, selector 0x08
+    dq 0x00AF9A000000FFFF
 .data equ $ - gdt64
-    dq 0x00CF92000000FFFF       ; kernel data, selector 0x10
+    dq 0x00CF92000000FFFF
 .user_code equ $ - gdt64
-    dq 0x00AFFA000000FFFF       ; user code, selector 0x18 / RPL3=0x1B
+    dq 0x00AFFA000000FFFF
 .user_data equ $ - gdt64
-    dq 0x00CFF2000000FFFF       ; user data, selector 0x20 / RPL3=0x23
+    dq 0x00CFF2000000FFFF
 .tss_low equ $ - gdt64
-times 16 db 0
+    times 16 db 0
 .pointer:
     dw $ - gdt64 - 1
     dq gdt64
 
 section .bss
 align 4096
+global page_table_l4
 page_table_l4: resq 512
+global page_table_l3
 page_table_l3: resq 512
-page_table_l2: resq 512
+global page_table_l2
+page_table_l2: resq 512 * 4
 
 align 16
 stack_bottom: resb 16384
+global stack_top
 stack_top:
 
 align 16
@@ -140,8 +134,7 @@ global tss64
 tss64:
     resb 104
 
-align 4
-multiboot_magic: resd 1
-multiboot_info:  resd 1
+align 8
+boot_info_ptr: resq 1
 
 section .note.GNU-stack noalloc noexec nowrite progbits
